764321d288d106e3af6e4ef78b718f8c2501f068
Rohansaini1512 <2022kucp1050@iiitkota.ac.in>
2024-06-19 02:04:30 -0400

    Adding .gitignore

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
+// import { connect } from 'mongoose';
 import app from './app.js';
+import connectionToDB from './config/dbConfig.js';
 
 const PORT = process.env.PORT || 5010;
 
 app.listen(PORT, async() => {
+    await connectionToDB();
     console.log(`App is running at http:localhost:${PORT}`);
 });
